7a779b216bc7501d6df9f30b8d6f32ed1c67f928
IDJPunkI <138734945+IDJPunkI@users.noreply.github.com>
2025-02-08 03:14:57 -0800

    Unit 1 almost done

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -5,6 +5,14 @@ using UnityEngine;
 public class PlayerController : MonoBehaviour
 {
     private Rigidbody rbPlayer;
+    private Vector3 direction = Vector3.zero;
+
+    [SerializeField]
+    private float forceMultiplier = 1.0f;
+
+    [SerializeField]
+    private ForceMode forceMode;
+    public GameObject spawnPoint;
 
     // Start is called before the first frame update
     void Start()
@@ -12,11 +20,39 @@ public class PlayerController : MonoBehaviour
         rbPlayer = GetComponent<Rigidbody>();
     }
 
+    void Update()
+    {
+        float horizontalVelocity = Input.GetAxis("Horizontal");
+        float verticalVelocity = Input.GetAxis("Vertical");
+
+        direction = new Vector3(horizontalVelocity, 0, verticalVelocity);
+    }
+
     // Update is called once per frame
     void FixedUpdate()
     {
-        float horizontalVelocity = Input.GetAxis("Horizontal");
+        rbPlayer.AddForce(direction * forceMultiplier, forceMode);
 
-        rbPlayer.AddForce(horizontalVelocity, 0, 0, ForceMode.Impulse);
+        if (transform.position.z > 38)
+        {
+            transform.position = new Vector3(transform.position.x, transform.position.y, 38);
+        }
+        else if (transform.position.z < -38)
+        {
+            transform.position = new Vector3(transform.position.x, transform.position.y, -38);
+        }
+    }
+
+    private void Respawn()
+    {
+        rbPlayer.MovePosition(spawnPoint.transform.position);
+    }
+
+    void OnTriggerExit(Collider collider)
+    {
+        if (collider.CompareTag("Hazard"))
+        {
+            Respawn();
+        }
     }
 }
