c1d84aae79bba94150bb011fbda92c80ef317797
IDJPunkI <138734945+IDJPunkI@users.noreply.github.com>
2025-04-10 00:46:38 -0700

    Unit 4 Halfway

---
+++ Assets/Scripts/Forest/HivePickUp.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class HivePickUp : MonoBehaviour
+{
+    public delegate void PickUpHive();
+    public static event PickUpHive HivePickedUp;
+
+    private void Start()
+    {
+        //NavPlayerMovement.DroppedHive += OnHiveDrop;
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if(other.CompareTag("Player"))
+        {
+            HivePickedUp?.Invoke();
+            gameObject.SetActive(false);
+        }
+    }
+
+    void OnHiveDrop(Vector3 position)
+    {
+        transform.position = position;
+        gameObject.SetActive(true);
+    }
+}
